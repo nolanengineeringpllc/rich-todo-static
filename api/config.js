518f5dcfda337e15c800f53ccfd8a01f0953ed75
nolanengineeringpllc <jocelyn@nolan-engineering.com>
2025-10-16 14:43:21 -0400

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,4 @@
+// This file stores your Google Apps Script endpoint for the to-do dashboard.
+// You can update this single line later if the Apps Script URL ever changes.
+
+window.API_URL = 'https://script.google.com/macros/s/AKfycbyb1nU9ryTTaOVzPezOdFLCyoGMZsQ1SxYGRpa5mc65HfgIiq_KusXPfuvSTDO_lJqzUQ/exec';
